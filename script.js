13b6f95354b351d0f6c9285d89375811d81b238f
MarisMihailovs <marismihailovs@gmail.com>
2022-10-07 14:01:01 +0300

    add countdown page

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const btnStart = document.getElementById('StartGame');
 const resultsTable = document.querySelector('.Results');
 const gameGrid = document.querySelector('.grid');
 const countdown = document.getElementById('countdown-page');
+const countdownTimer = document.getElementById('countdown-timer');
 
 let gridofCards = [];
 let card = {};
@@ -12,7 +13,23 @@ let result = {};
 function launchGame() {
     resultsTable.style.display = "none";
     btnStart.style.display = "none";
-    countdown.style.display = "flex";
+    countdown.hidden = false;
+    countdownStart();
 }
+
+// displays 1,2,3 GO!
+function countdownStart() {
+    countdownTimer.textContent = '3';
+    setTimeout(() => {
+        countdownTimer.textContent = '2';
+    }, 1000);
+    setTimeout(() => {
+        countdownTimer.textContent = '1';
+    }, 2000);
+    setTimeout(() => {
+        countdownTimer.textContent = 'GO!';
+    }, 3000);
+}
+
 
 btnStart.addEventListener("click", launchGame);
