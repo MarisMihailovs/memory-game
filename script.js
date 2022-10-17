10c71fba767fb5f3d915a5f47e2e650f276fdf9c
MarisMihailovs <marismihailovs@gmail.com>
2022-10-17 17:42:24 +0300

    add card flip animation eventlisteners

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const resultsTable = document.querySelector('.Results');
 const gameGrid = document.querySelector('.grid');
 const countdown = document.getElementById('countdown-page');
 const countdownTimer = document.getElementById('countdown-timer');
+const cardGrid = document.querySelectorAll(".card");
 
 let gridofCards = [];
 let card = {};
@@ -16,6 +17,23 @@ function launchGame() {
     countdown.hidden = false;
     countdownStart();
 }
+
+function hoverCard() {
+    console.log(this, "over");
+    this.classList.add("hoverCard");
+}
+
+function outCard() {
+    console.log(this, "out");
+    this.classList.remove("hoverCard");
+}
+
+function selectCard() {
+    console.log(this);
+    this.classList.toggle("selected");
+    console.log(this.classList);
+}
+
 
 // displays 1,2,3 GO!
 function countdownStart() {
@@ -38,3 +56,6 @@ function countdownStart() {
 
 
 btnStart.addEventListener("click", launchGame);
+cardGrid.forEach(cardGrid => cardGrid.addEventListener("click", selectCard));
+cardGrid.forEach(cardGrid => cardGrid.addEventListener("mouseover", hoverCard));
+cardGrid.forEach(cardGrid => cardGrid.addEventListener("mouseleave", outCard));
