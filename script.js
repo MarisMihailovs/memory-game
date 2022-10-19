695a913703ca2af313b2b07ef85a316d75efae36
MarisMihailovs <marismihailovs@gmail.com>
2022-10-19 18:23:06 +0300

    add logo and shuffle

--- script.js
+++ script.js
@@ -5,9 +5,27 @@ const grid = document.querySelector('.grid');
 const countdown = document.getElementById('countdown-page');
 const countdownTimer = document.getElementById('countdown-timer');
 const cards = document.querySelectorAll(".flipContainer");
+const cardsBack = document.querySelectorAll(".cardBack");
 
+let gridofCards = [
+    "images/tiles/Barco.jpg",
+    "images/tiles/Evoko.jpg",
+    "images/tiles/JOAN.jpg",
+    "images/tiles/Logitech.jpg",
+    "images/tiles/MB.jpg",
+    "images/tiles/MI.jpg",
+    "images/tiles/owllabs.jpg",
+    "images/tiles/samsung.jpg",
+    "images/tiles/Barco.jpg",
+    "images/tiles/Evoko.jpg",
+    "images/tiles/JOAN.jpg",
+    "images/tiles/Logitech.jpg",
+    "images/tiles/MB.jpg",
+    "images/tiles/MI.jpg",
+    "images/tiles/owllabs.jpg",
+    "images/tiles/samsung.jpg"
+];
 
-let gridofCards = [];
 let resultTable = [];
 let result = {};
 
@@ -33,6 +51,12 @@ function selectCard() {
     this.classList.toggle("selected");
 }
 
+function fillCards() {
+    shuffle(gridofCards);
+    cardsBack.forEach((element, index) => {
+        element.style.backgroundImage = `url(${gridofCards[index]})`;
+    })
+}
 
 // displays 1,2,3 GO!
 function countdownStart() {
@@ -48,6 +72,7 @@ function countdownStart() {
     }, 3000);
     setTimeout(() => {
         countdown.hidden = true;
+        fillCards();
         gameArea.style.display = "inline";
         grid.style.display = "flex";
     }, 3500);
