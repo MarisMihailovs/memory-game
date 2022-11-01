adcd88301e22f0ddeb2bb7e8aa4af62f878c1d8e
MarisMihailovs <marismihailovs@gmail.com>
2022-11-01 10:02:54 +0200

    add high ranking table and save to localstorage

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 const gameScreen = document.getElementsByTagName("BODY")[0];
 const btnStart = document.getElementById('StartGame');
-const resultsTable = document.querySelector('.Results');
-const gameArea = document.querySelector('.gameArea');
-const grid = document.querySelector('.grid');
+// countdown 
 const countdown = document.getElementById('countdown-page');
 const countdownTimer = document.getElementById('countdown-timer');
+// Game
+const gameArea = document.querySelector('.gameArea');
+const grid = document.querySelector('.grid');
 const cards = document.querySelectorAll(".flipContainer");
 const cardsBack = document.querySelectorAll(".cardBack");
 const time = document.querySelector('.time');
+// modal buttons
+const highScoresBtn = document.getElementById('highScoresBtn');
+const playAgainBtn = document.getElementById('playAgainBtn');
+// fullscreen mode toggle
 const fullscreenBtn = document.querySelector('.fullscreen');
+// results table node
+const resultsTable = document.querySelector('.Results');
+const player = document.querySelectorAll('h3');
+// highscore modal elements
+const modal = document.querySelector('.modal-container');
+const modalClose = document.getElementById('close-modal');
+const resultTime = document.getElementById('resultTime');
+const playerName = document.getElementById('player-Name');
+const bookmarkForm = document.getElementById('bookmark-form');
 
 let gridofCards = [
     {
@@ -117,8 +131,8 @@ let card1Index;
 let card2Index;
 
 let playerGuessCount = 0;
-let resultTable = [];
-let timePlayed;
+let highScoreArray = [];
+let timePlayed = 0;
 let timer;
 let result = {};
 
@@ -154,21 +168,12 @@ function closeFullscreen() {
     gameScreen.classList.remove('game-fullscreen');
 }
 
-//stop timer and process results
-function checkTime() {
-    if (playerGuessCount === 16) {
-        clearInterval(timer);
-    }
-}
-
 // add tenth of a second to timePlayed
 function addTime() {
     timePlayed += 0.1;
     time.textContent = `${timePlayed.toFixed(1)}`
     checkTime();
 }
-
-
 
 // start timer when game starts
 function startTimer() {
@@ -177,6 +182,93 @@ function startTimer() {
     timer = setInterval(addTime, 100);
 }
 
+//stop timer and process results
+function checkTime() {
+    if (playerGuessCount === 16) {
+        clearInterval(timer);
+    }
+}
+
+// show modal and save highScore
+function showModal() {
+    checkTime();
+    modal.classList.add('show-modal');
+    if (time.textContent < highScoreArray[9].Time) {
+        resultTime.innerText = `Congrats! Your time is ${time.textContent}s! You have made it to high-scores`;
+        bookmarkForm.hidden = false;
+        playerName.focus();
+    } else {
+        highScoresBtn.style.display = "inline-block";
+        playAgainBtn.style.display = "inline-block";
+        bookmarkForm.hidden = true;
+        resultTime.innerText = `Your time is ${time.textContent}s! Good luck next time! Play again?`;
+    }
+}
+
+function closeModal() {
+    modal.classList.remove('show-modal');
+}
+
+// reset game grid
+function resetGameGrid() {
+    if (playerGuessCount == 16) {
+        cards.forEach((element) => {
+            element.classList.remove("selected");
+        });
+
+        gridofCards.forEach((element) => {
+            element.guessed = false;
+            element.state = "closed";
+        })
+        gameArea.style.display = "none";
+        grid.style.display = "none";
+        closeModal();
+        cardsOpen = 0;
+        timePlayed = 0;
+        playerGuessCount = 0;
+    }
+}
+// read highScore table
+function showHighScores() {
+    resetGameGrid();
+    resultsTable.style.display = "flex";
+    btnStart.style.display = "flex";
+    highScoreArray.forEach((element, index) => {
+        player[index].innerText = `${element.Name} - ${element.Time}s`;
+    });
+}
+
+function getHighScores() {
+    if (localStorage.getItem('highScores')) {
+        highScoreArray = JSON.parse(localStorage.highScores);
+        console.log(highScoreArray);
+    } else {
+        highScoreArray = [
+            { Name: 'Player1', Time: 100 },
+            { Name: 'Player2', Time: 100 },
+            { Name: 'Player3', Time: 100 },
+            { Name: 'Player4', Time: 100 },
+            { Name: 'Player5', Time: 100 },
+            { Name: 'Player6', Time: 100 },
+            { Name: 'Player7', Time: 100 },
+            { Name: 'Player8', Time: 100 },
+            { Name: 'Player9', Time: 100 },
+            { Name: 'Player10', Time: 100 },
+        ];
+        highScoreArray.sort((e1, e2) => (e1.Time > e2.Time) ? 1 : (e1.Time < e2.Time) ? -1 : 0);
+        localStorage.setItem('highScores', JSON.stringify(highScoreArray));
+    }
+
+    console.log(highScoreArray);
+    console.log(localStorage.getItem('highScores'));
+    showHighScores();
+}
+
+
+function playAgain() {
+    resetGameGrid();
+    launchGame();
+}
 
 
 function launchGame() {
@@ -224,7 +316,7 @@ function selectCard() {
 
 function checkIfWon() {
     if (playerGuessCount === 16) {
-        console.log("You won!")
+        showModal()
     }
 }
 
@@ -244,10 +336,9 @@ function checkCard() {
                     cards[i].classList.remove('selected');
                     gridofCards[i].state = "closed";
                 }
-
             }
             cardsOpen = 0;
-        }, 2000);
+        }, 1500);
     }
 }
 
@@ -280,12 +371,48 @@ function countdownStart() {
         fillCards();
         gameArea.style.display = "inline";
         grid.style.display = "flex";
+        highScoresBtn.style.display = "none";
+        playAgainBtn.style.display = "none";
     }, 3500);
 }
 
+function checkPlace(result) {
+
+    for (let i = 0; i < highScoreArray.length; i++) {
+        return result.Time < highScoreArray[i].Time;
+    };
+}
+
+function storeResult(e) {
+    e.preventDefault();
+    const nameValue = playerName.value;
+    result = { Name: nameValue, Time: time.textContent };
+    console.log(result);
+    console.log(highScoreArray[9].Time);
+    if (result.Time < highScoreArray[9].Time) {
+        highScoreArray.push(result);
+        highScoreArray.sort((e1, e2) => (e1.Time > e2.Time) ? 1 : (e1.Time < e2.Time) ? -1 : 0);
+        highScoreArray.length = 10;
+        localStorage.setItem('highScores', JSON.stringify(highScoreArray));
+        result = [];
+        bookmarkForm.reset();
+        closeModal();
+        showHighScores();
+    }
+    console.log(highScoreArray);
+    console.log(localStorage.getItem('highScores'));
+}
 
 btnStart.addEventListener("click", launchGame);
 cards.forEach(cards => cards.addEventListener("click", selectCard));
 cards.forEach(cards => cards.addEventListener("mouseover", hoverCard));
 cards.forEach(cards => cards.addEventListener("mouseleave", outCard));
 fullscreenBtn.addEventListener('click', toggleFullscreen);
+highScoresBtn.addEventListener('click', showHighScores);
+playAgainBtn.addEventListener('click', playAgain);
+bookmarkForm.addEventListener('submit', storeResult);
+
+
+
+// get highScores on load
+getHighScores();
